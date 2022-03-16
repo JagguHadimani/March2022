d490016c694f9826f78911d7a9d6b7b327fb3cae
Jagadish Hadimani <jagadishhadimani@hotmail.com>
2022-03-16 20:53:28 +0530

    Following Brian's Tutorial

--- New Project/src/Selenium/AllLinksonWebpage.java
+++ New Project/src/Selenium/AllLinksonWebpage.java
@@ -1,5 +1,6 @@
 package Selenium;
 
+import java.util.Iterator;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -15,8 +16,21 @@ public class AllLinksonWebpage {
 		System.setProperty("webdriver.gecko.driver", "D:\\Softwares\\drivers\\geckodriver\\geckodriver.exe");
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.amazon.in/");
+		
+		String url = "";
 		List<WebElement> links = driver.findElements(By.tagName("a"));
 		System.out.println("No of links on this page are: " + links.size());
+		
+		//We will iterate through the list and will check the elements in the list using Iterator.
+		Iterator<WebElement> itr =  links.iterator();
+		while itr.hasNext()){
+			url=itr.next().getText(); //prints the link text.
+		}
+		
+		//for (int i = 0; i < links.size(); i++) {
+			
+
+		}
 
 	}
 
